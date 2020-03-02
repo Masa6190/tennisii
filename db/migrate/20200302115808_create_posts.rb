4d7525335dc7b87e95bda7705dd6860b9818dd3f
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-03-03 00:25:35 +0900

    first day

---
+++ db/migrate/20200302115808_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :title
+      t.text :main
+      t.text :place
+      t.integer :person
+      t.datetime :starttime
+      t.integer :money
+      t.timestamps
+    end
+  end
+end
