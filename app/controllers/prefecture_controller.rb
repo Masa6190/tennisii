d97612cc1eeb97c8ef73f0565eee4280e4707e4d
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-27 12:36:12 +0900

    Update prefecture_controller.rb

--- app/controllers/prefecture_controller.rb
+++ app/controllers/prefecture_controller.rb
@@ -3,7 +3,8 @@ class PrefectureController < ApplicationController
   end
 
   def tokyo
-    @post = Post.where(prefecture: "東京都")
+    @post = Post.where(prefecture: "東京都").order("id DESC")
+    @prefecture = "東京"
     render action: :show
   end
 
