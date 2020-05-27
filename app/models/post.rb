bcbf722da2c3399518af4d3ff8542a40d5ac4617
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-27 18:52:12 +0900

    destroyと子のコメントも一緒に削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :title, :main, :Prefecture, :person, :starttime, :money, :user_id, presence: true
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
