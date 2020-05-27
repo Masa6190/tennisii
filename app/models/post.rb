cee899a3fafd09b714f0351895bd575452baf52f
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-28 01:11:54 +0900

    scope追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   validates :title, :main, :Prefecture, :person, :starttime, :money, :user_id, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  scope :search, -> (search){ where('main LIKE(?)', "%#{search}%") }
+
 end
