8b30da547c7e7a5d5099506b0771a83287d8da2d
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-29 15:55:26 +0900

    バリデーション修正

--- db/migrate/20200525135140_create_comments.rb
+++ db/migrate/20200525135140_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.bigint :user_id
-      t.bigint :post_id
-      t.text :text
+      t.bigint :user_id, null: false
+      t.bigint :post_id, null: false
+      t.text :text, null: false
       t.timestamps
     end
   end
